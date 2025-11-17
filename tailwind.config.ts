a871f59e2692df6078f1bb2ab4efa6b919cad7ee
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-17 17:25:06 +0000

    use font style only poppins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Belleza', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
