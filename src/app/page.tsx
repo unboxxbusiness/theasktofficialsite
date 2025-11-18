5ec7dcf8163d9b9b7d99554ef9765f8c3a26a94d
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-18 06:01:55 +0000

    make hero section with this design "use client";
    import { motion } fro

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "./pages/components/hero-section";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <HeroSection />
+    </main>
+  );
+}
