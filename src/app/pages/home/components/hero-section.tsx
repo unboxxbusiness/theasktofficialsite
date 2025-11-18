f5b0cf5fa398d0ca1e12c72c5997943d0b864a66
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-18 06:28:29 +0000

    change this into Enroll Now (_for element button_)

--- src/app/pages/home/components/hero-section.tsx
+++ src/app/pages/home/components/hero-section.tsx
@@ -108,8 +108,8 @@ const Navbar = () => {
         <div className="size-7 rounded-full bg-gradient-to-br from-primary via-accent to-secondary" />
         <h1 className="text-base font-bold md:text-2xl">TheAskt</h1>
       </div>
-      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-        Login
+      <button className="w-32 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+        Enroll Now
       </button>
     </nav>
   );
