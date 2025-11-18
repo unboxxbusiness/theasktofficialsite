b0d5b938c4d5b03cdc6f3c4bcac8b5f2c1f8a91a
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-18 06:29:17 +0000

    its should be single line not like double line (_for element button_)

--- src/app/pages/home/components/hero-section.tsx
+++ src/app/pages/home/components/hero-section.tsx
@@ -108,7 +108,7 @@ const Navbar = () => {
         <div className="size-7 rounded-full bg-gradient-to-br from-primary via-accent to-secondary" />
         <h1 className="text-base font-bold md:text-2xl">TheAskt</h1>
       </div>
-      <button className="w-32 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+      <button className="transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Enroll Now
       </button>
     </nav>
