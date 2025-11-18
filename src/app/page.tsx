caa8a5ec3ee7bfc3993ce2c856d37bdaeb02c8b7
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-18 06:10:01 +0000

    can you make sure its visiblility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./pages/home/components/hero-section";
+import StatsSection from "./pages/home/components/stats-section";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center">
       <HeroSection />
+      <StatsSection />
     </main>
   );
 }
