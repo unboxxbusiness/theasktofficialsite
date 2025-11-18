d6732c5f128cb29777108c1a719d3922cc6ac7cf
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-18 08:53:09 +0000

    this section will be come after why theaskt exists (_for element div_)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,8 +13,8 @@ export default function AboutPage() {
       </div>
       <AboutHero />
       <AboutContent />
-      <WhatMakesUsDifferent />
       <WhyTheasktExists />
+      <WhatMakesUsDifferent />
       <Footer />
     </main>
   );
