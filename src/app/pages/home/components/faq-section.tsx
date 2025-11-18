3045902af0756b43ba9b81fbdda6949d1947efeb
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-18 08:11:55 +0000

    make faq as per need to resolve this build error

--- src/app/pages/home/components/faq-section.tsx
+++ src/app/pages/home/components/faq-section.tsx
@@ -7,7 +7,6 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion'
-import { Button } from '@/components/ui/button'
 import { PhoneCall } from 'lucide-react'
 
 export default function FAQSection() {
@@ -79,10 +78,8 @@ export default function FAQSection() {
                 about our platform, services, and features.
               </p>
             </div>
-            <Link href="#">
-              <Button className="gap-4" variant="outline">
+            <Link href="#" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 gap-4">
                 Any questions? Reach out <PhoneCall className="w-4 h-4" />
-              </Button>
             </Link>
           </div>
           <Accordion type="single" collapsible className="w-full">
